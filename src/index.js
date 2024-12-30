ad8925ec576ddaec3f4e6368617adad6750bfa20
干净 <54979375+du5@users.noreply.github.com>
2024-12-30 12:38:47 +0800

    添加 ethers 合约调用以获取用户签名地址的点数，并更新 fetch 函数以处理请求参数

--- src/index.js
+++ src/index.js
@@ -10,10 +10,52 @@
 
 import { ethers } from 'ethers'
 
+let provider = new ethers.JsonRpcProvider('https://0.48.club')
+
+const spAbi = [
+	"function getPoint(address) public view returns (uint256)"
+];
+
+const spContract = new ethers.Contract("0x928dC5e31de14114f1486c756C30f39Ab9578A92", spAbi, provider);
+
+const minAllowPoint = BigInt(48);
+
+async function checkUserSP(address) {
+	return await spContract.getPoint(address) >= minAllowPoint;
+}
+
+function errorCode400() {
+	return new Response('R U OK?', { status: 400 });
+}
 
 export default {
 	async fetch(request, env, ctx) {
-		let provider = new ethers.JsonRpcProvider(env.CF_BSC_RPC)
-		return new Response(JSON.stringify(await provider.getNetwork()), { status: 200 })
+		let url = new URL(request.url);
+		if (url.searchParams.size !== 3 || url.pathname !== '/') {
+			return errorCode400();
+		}
+
+		console.log(url.searchParams.size);
+		let sig = url.searchParams.get('sig');
+		let msg = JSON.stringify({
+			file: url.searchParams.get('file'),
+			tt: url.searchParams.get('tt'),
+		})
+		console.log(msg);
+
+
+		// @TODO: 验证签名
+		if (!sig) {
+			// 签名不合规
+			return errorCode400();
+		}
+
+		if (!(await checkUserSP('0x34a02F8706d8859F5cDd4a7620F26E12Ef23D168'))) {
+			// 用户 SP 不足
+			return errorCode400();
+		}
+
+		// 返回用户请求的 $file 的内容
+		return new Response("xixi~", { status: 200 })
 	},
 };
